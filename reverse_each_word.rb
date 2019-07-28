7a32285d8a35344be84f58dde280cb3a41c9960a
George Kirby <georgeskirby@btinternet.com>
2019-07-28 15:00:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
 def reverse_each_word(given_sentence)
   sentence_as_array = given_sentence.split
   
-  reversed_sentence_as_array = sentence_as_array.collect {
-  
-  sentence_as_array.each do
-    |item| reversed_sentence_as_array.push(item.reverse)
-  end
+  reversed_sentence_as_array = sentence_as_array.collect {|i|i.reverse}
   
   reversed_sentence = reversed_sentence_as_array.join(" ")
   
